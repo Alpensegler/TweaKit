fb261c312cf6dfebe5fa11d962f03ff27a711996
Cokile <kelvintgx@gmail.com>
2021-02-10 11:56:41 +0800

    chore(project): Initial project

---
+++ Demo/Demo/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  TweaKit-Demo
+//
+//  Created by cokile on 2021/2/9.
+//
+// swiftlint:disable all
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        true
+    }
+}
---
+++ Demo/Demo/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  TweaKit-Demo
+//
+//  Created by cokile on 2021/2/9.
+//
+
+import UIKit
+import TweaKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
